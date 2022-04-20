44969b8d1ddce3364ea8e6c387c75cada37c8e09
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-20 12:09:47 +0800

    test

--- lesson1/task2/src/UserMenu.jsx
+++ lesson1/task2/src/UserMenu.jsx
@@ -3,15 +3,17 @@ import { UserContext } from './context';
 
 class UserMenu extends Component {
   render() {
-    console.log(this.context)
+    console.log(this.context);
     return (
-      <div className='menu__greeting'>
-        <span>Hello, {this.context.name}</span>
-        <img
-          alt='User Avatar'
-          src={this.context.avatar_url}
-          className='menu__avatar'
-        />
+      <div class="menu">
+        <div className='menu__greeting'>
+          <span>Hello, {this.context.name}</span>
+          <img
+            alt='User Avatar'
+            src={this.context.avatar_url}
+            className='menu__avatar'
+          />
+        </div>
       </div>
     );
   };
