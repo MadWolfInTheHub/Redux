33a7db8955f6ad5df62325486b607b830eb4b98d
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-20 12:04:17 +0800

    test

--- lesson1/task2/src/App.jsx
+++ lesson1/task2/src/App.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import Header from "./header";
-import { userData, UserContext } from './context';
+import { UserContext } from './context';
 
 class App extends Component{
-  state ={
-    userData,
-  }
+  state = {
+    userData: {
+      name: 'Nicola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001'
+    },
+  };
   
   render() {
     return (
       <div className="page">
+      <UserContext.Provider value={this.state.userData}>
         <Header />
+      </UserContext.Provider>
       </div>
     );
   }
--- lesson1/task2/src/context.js
+++ lesson1/task2/src/context.js
@@ -1,7 +1,3 @@
 import React from 'react';
-export const userData = {
-  name: 'Nicola Tesla',
-  avatar_url: 'https://avatars3.githubusercontent.com/u10001'
-}
 
-export const UserContext = React.createContext(userData);
+export const UserContext = React.createContext({});
