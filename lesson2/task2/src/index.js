d8566a26413ed860b8dcfe4c6912741f4ade7932
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-20 17:38:41 +0800

    Counter + history - Redux

---
+++ lesson2/task2/src/index.js
@@ -0,0 +1,31 @@
+import store, { increment, decrement, reset } from './store.js';
+import './index.scss';
+
+const resultElem = document.querySelector('.counter__result');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const resetBtn = document.querySelector('[data-action="reset"]');
+
+const onIncrement = () => {
+  store.dispatch(increment());
+};
+
+const onDecrement = () => {
+  store.dispatch(decrement());
+};
+
+const onReset = () => {
+  store.dispatch(reset());
+};
+
+incrementBtn.addEventListener('click', onIncrement);
+decrementBtn.addEventListener('click', onDecrement);
+resetBtn.addEventListener('click', onReset);
+
+store.subscribe(() => {
+  
+  const state = store.getState()
+  const currentValue = state.history.length === 0 ? null : state.history.reduce((acc, value) => acc + value);
+  const historyString = state.history.join(' '); 
+  resultElem.textContent = state.history.length === 0 ? '' : `${historyString} = ${currentValue}`;
+});
