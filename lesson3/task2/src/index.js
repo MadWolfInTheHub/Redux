409593a301639788d67b14c2ef32dd4c581562df
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-21 20:37:48 +0800

    test

--- lesson3/task2/src/index.js
+++ lesson3/task2/src/index.js
@@ -2,8 +2,3 @@ import store from "./counter";
 import { increment, decrement, reset } from "./counter";
 
 store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(increment());
-store.dispatch(increment());
