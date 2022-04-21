05c37d87a44ccd3defdd6d73d9ddcec73c429b08
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-21 20:32:02 +0800

    test

--- lesson3/task1/src/store.js
+++ lesson3/task1/src/store.js
@@ -8,8 +8,9 @@ const appReducer = combineReducers({
 })
 
 const store = createStore(
-  
   appReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && 
+  window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 export default store;
---
+++ lesson3/task2/src/index.js
@@ -0,0 +1,9 @@
+import store from "./counter";
+import { increment, decrement, reset } from "./counter";
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
+store.dispatch(increment());
