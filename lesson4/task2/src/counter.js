76727ebf4232cced283a1b29a3f62ebce199cce9
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-22 15:43:36 +0800

    test

---
+++ lesson4/task2/src/counter.js
@@ -0,0 +1,38 @@
+import { createStore } from 'redux';
+export const INCREMENT = 'COUNTER/INCREMENT';
+export const DECREMENT = 'COUNTER/DECREMENT';
+
+
+export const increment = () => {
+  return {
+    type: INCREMENT
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT
+  };
+};
+
+const initialState = 0
+
+export const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+      
+    case DECREMENT:
+      return state - 1;
+
+    default:
+      return state;
+  };
+};
+
+export const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && 
+  window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
---
+++ lesson4/task3/src/index.js
@@ -0,0 +1,4 @@
+import {store} from "./counter";
+import { increment, decrement, reset } from "./counter";
+
+store.subscribe(() => console.log(store.getState()));
