15c52de8aef3d52b14e669533dc804c27a6cf746
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-24 17:43:15 +0800

    test

--- lesson6/task3/src/App.jsx
+++ lesson6/task3/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from 'react-redux';
 import store from "./store";
-import Weather from "./wheather/Weather";
+import Weather from "./weather/Weather";
 
 const App = () => {
   return (
